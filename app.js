3a233682b3e1e894aab75b4c23973a2a1ddf7e70
ErikkGonzalesz3 <gonzales.erik3535@gmail.com>
2025-01-20 18:54:16 -0300

    Crea el app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; 
+let nombres;
+
+function add(){
+    nombres = document.getElementById('amigo1').value;
+
+    if (nombres == ""){
+        alert("Por favor, inserte un nombre");
+    }else{
+
+        amigos.push(nombres);
+
+        document.querySelector('#amigo1').value = "";
+
+        actualizarLista();
+    }
+}
+
+function actualizarLista(){
+
+    let lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = ""; //asegurarse que no haya duplicados al actualizar
+
+    for ( let i = 0; i < amigos.length ; i++  ){
+
+        lista.innerHTML += ` <li> ${amigos[i]} </li> `;
+
+    }
+
+
+}
